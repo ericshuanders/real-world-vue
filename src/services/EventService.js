faca2fa4a21d33619aac1205c057599128ba67c2
Eric <ericshuanders@gmail.com>
2021-01-28 10:46:14 -0500

    L5-start refactored axios call

---
+++ src/services/EventService.js
@@ -0,0 +1,19 @@
+import axios from 'axios'
+
+//below, we are creating the single Axios instance we can 
+//use across our entire app
+const apiClient = axios.create({
+    baseURL: "https://my-json-server.typicode.com/ericshuanders/real-world-vue",
+    withCredentials: false,
+    headers:{
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default{
+    getEvents(){
+        return apiClient.get('/events')
+        //this is added on to the base url
+    }
+}
